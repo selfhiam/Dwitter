d235e955d5508f1c1b0590fca6d8715df8ffcff5
selfhiam <selfhiam@naver.com>
2023-11-03 11:14:53 +0900

    validation 적용

--- router/tweets.js
+++ router/tweets.js
@@ -1,8 +1,18 @@
 import express from "express";
 import * as tweetController from '../controller/tweet.js';
+import { body } from 'express-validator';
+import { validate } from "../middleware/validator.js";
 
 // 라우터 객체 생성
 const router = express.Router();
+
+/* 
+    POST, PUT에 text부분에 빈문자열을 없애고, 최소 3자이상 입력해야 저장되도록 API에 적용
+*/
+const validateTweet = [
+    body('text').trim().isLength({ min: 3}).withMessage('최소 3자 이상 입력해!'), validate
+];
+
 
 // GET / tweets 
 // GET / tweets?username=:username
@@ -31,7 +41,7 @@ router.get('/:id', tweetController.getTweet);
 
 
 // POST / tweets
-router.post('/', tweetController.createTweet);
+router.post('/', validateTweet, tweetController.createTweet);
 // (req, res, next) => {
 //     const { text, name, username } = req.body;
 //     const tweet = {
@@ -47,7 +57,7 @@ router.post('/', tweetController.createTweet);
 
 
 // PUT / tweets/:id
-router.put('/:id', tweetController.updateTweet);
+router.put('/:id', validateTweet, tweetController.updateTweet);
 // (req, res, next) => {
 //     const id = req.params.id;
 //     const text = req.body.text;
