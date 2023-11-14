e6acac5cfc1b6e8b62097f8b794aaaa151368c10
selfhiam <selfhiam@naver.com>
2023-11-14 11:27:05 +0900

    sequelize 적용

--- data/auth.js
+++ data/auth.js
@@ -1,16 +1,49 @@
-import { db } from '../db/database.js';
+import SQ, { DATE } from 'sequelize';
+import { sequelize } from '../db/database.js';
+
+const DataTypes = SQ.DataTypes;
+
+// orm, odm은 자동으로 table이름뒤에 s를 붙혀줌
+// 테이블이 있으면 요놈들이 생성이 안됨.
+// 하지만 테이블이 없으면 자동으로 생성이됨
+export const User = sequelize.define(
+    'user',
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        username: {
+            type: DataTypes.STRING(45),
+            allowNull: false
+        },
+        password: {
+            type: DataTypes.STRING(128),
+            allowNull: false
+        },
+        name: {
+            type: DataTypes.STRING(128),
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING(128),
+            allowNull: false
+        },
+        url: DataTypes.TEXT
+    },
+    { timestamps: false }
+)
 
 export async function findByUsername(username) {
-    return db.execute('SELECT * from users where username=?', [username])
-    .then((result) => result[0][0]);
+    return User.findOne({ where: { username }});
 };
 
 export async function findById(id) {
-    return db.execute('SELECT * FROM users WHERE id=?', [id])
-    .then((result) => result[0][0])
+    return User.findByPk(id);
 };
 
 export async function createUser(user) {
-    const { username, password, name, email, url } = user;
-    return db.execute('INSERT INTO users (username, password, name, email, url) VALUES (?, ?, ?, ?, ?)', [username, password, name, email, url]).then((result) => result[0].insertId);
+    return User.create(user).then((data) => data.dataValues.id);
 };
